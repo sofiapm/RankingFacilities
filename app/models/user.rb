e3bb80e7512ad92b60a0bf0a14ee6ce78ea1f038
Sofia Martins <mps.sofia@gmail.com>
2014-12-12 14:05:55 +0000

    adicionados testes do modelo users para first_name, last_name, email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
     include RoleModel
@@ -13,7 +13,10 @@ class User < ActiveRecord::Base
 	belongs_to :address, class_name: 'Address', foreign_key: 'address_id'
 
 	accepts_nested_attributes_for :address, :roles
-	
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email, presence: true
+
 	def full_name
 		first_name + " " + last_name
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  	test "a user should enter a first name" do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:first_name].empty?
+	end 
+
+	test "a user should enter a last name" do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:last_name].empty?
+	end 
+
+	test "a user should enter an email" do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:email].empty?
+	end 
+
+	test "a user should have an unique email" do
+		user = User.new
+		user.email = users(:joana).email
+
+		assert !user.save
+		puts user.errors.inspect
+		assert !user.errors[:email].empty?
+	end 
 end
