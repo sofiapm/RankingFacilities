051d2a00aa481288e70ef14c95d35f743a4ef57d
Sofia Martins <mps.sofia@gmail.com>
2014-12-12 14:33:17 +0000

    adicionado teste para verificar tamanho de zip_code

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -24,4 +24,20 @@ class AddressTest < ActiveSupport::TestCase
 		assert !address.save
 		assert !address.errors[:zip_code].empty?
 	end 
+
+	test "a user should enter a zip_code with 4 digits not less" do
+		address = Address.new
+		address.zip_code = addresses(:joana_less_than_four).zip_code
+		assert !address.save
+		puts address.errors.inspect
+		assert !address.errors[:zip_code].empty?
+	end 
+
+	test "a user should enter a zip_code with 4 digits not more" do
+		address = Address.new
+		address.zip_code = addresses(:joana_great_than_four).zip_code
+		assert !address.save
+		puts address.errors.inspect
+		assert !address.errors[:zip_code].empty?
+	end 
 end
