5fe10f0a4fa361f0dd6bb383d9ef9b4159d6aadb
Sofia Martins <mps.sofia@gmail.com>
2015-02-04 18:54:14 +0000

    adicionados files que nao eram css ao assets.rb precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( bootstrap-datepicker.js, bootstrap.js, bootstrap.min.js, bootstrap-theme.min.css, bootstrap.min.css, bootstrap.css.map.css, bootstrap.css.map, bootstrap-theme.css.map.css, bootstrap-theme.css.map )
